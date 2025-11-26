0109ba7f519088d46d52cb4542aa35bbf55ed91f
Roman Kazmin <kazmin.roman@wb.ru>
2025-11-26 18:41:23 +0300

    Аргументы и опции

--- cmd/gendiff/main.go
+++ cmd/gendiff/main.go
@@ -13,6 +13,14 @@ func main() {
 		Name:      "gendiff",
 		Usage:     "compares two configuration files and shows a difference",
 		UsageText: "gendiff [global options]",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "format",
+				Aliases: []string{"f"},
+				Usage:   "output format (default: stylish)",
+				Value:   "stylish",
+			},
+		},
 		Action: func(ctx context.Context, cmd *cli.Command) error {
 			args := cmd.Args().Slice()
 
